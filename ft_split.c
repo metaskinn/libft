c0aa814e1fc2717988acb2917b7e64a323ad600b
Meltem Taskin <metaskin@k1m20s03.42istanbul.com.tr>
2026-01-10 17:25:35 +0300

    split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,62 @@
 /*   By: metaskin <metaskin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 00:48:20 by metaskin          #+#    #+#             */
-/*   Updated: 2026/01/09 02:55:51 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/01/09 12:51:27 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	wcount(char const *s, char c)
+{
+	size_t	n;
+
+	n = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+			n++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (n);
+}
+
+static char	**freee(char **a, size_t i)
+{
+	while (i)
+		free(a[--i]);
+	free(a);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char		**a;
+	size_t		i;
+	char const	*st;
+
+	if (!s)
+		return (NULL);
+	a = ft_calloc(wcount(s, c) + 1, sizeof(*a));
+	if (!a)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		st = s;
+		while (*s && *s != c)
+			s++;
+		if (s > st)
+		{
+			a[i] = ft_substr(st, 0, (size_t)(s - st));
+			if (!a[i++])
+				return (freee(a, i - 1));
+		}
+	}
+	return (a);
 }
